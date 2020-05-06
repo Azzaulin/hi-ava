4d80c21716340ab2a71e357febaaad4dcae91b93
Azzaulin <64099478+Azzaulin@users.noreply.github.com>
2020-05-06 15:27:23 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Isabella Intan Dhiah Pitaloka", "Baby", "Sayang", "Isabella Intan", "Isabella"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
